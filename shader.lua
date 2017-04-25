dbc7380416840f5b86f016c4a216e6c9032a8d6e
Christopher Moore <christopher.e.moore@gmail.com>
2017-04-25 16:15:46 +0300

    updating to match lua-gl

--- shader.lua
+++ shader.lua
@@ -1,11 +1,14 @@
 local ffi = require 'ffi'
 local gl = require 'ffi.OpenGLES2'
 local class = require 'ext.class'
+local showcode = require 'template.showcode'
 
 local GLShader = class()
+
 function GLShader:init(code)
 	self.id = gl.glCreateShader(self.type)
 	local len = ffi.new('int[1]')
+	len[0] = #code
 	local strs = ffi.new('const char*[1]')
 	strs[0] = code
 	gl.glShaderSource(self.id, 1, strs, len)
@@ -17,10 +20,12 @@ function GLShader:init(code)
 		local length = ffi.new('int[1]')
 		gl.glGetShaderiv(self.id, gl.GL_INFO_LOG_LENGTH, length)
 		local log = ffi.new('char[?]',length[0]+1)
-		local result = ffi.new('size_t[1]')
+		local result = ffi.new('GLsizei[1]')
 		gl.glGetShaderInfoLog(self.id, length[0], result, log);
+		print(showcode(code))
+		print('log:')
 		print(ffi.string(log))
-		error(code)
+		error("compile failed")
 	end 
 end
 
